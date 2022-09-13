b8e2e1cf9941e27e4be617fb070ceb81e6cd1cae
Lennart Mol <lc.mol@student.avans.nl>
2022-09-13 14:26:18 +0200

    GET request to get patientId

--- requestData.py
+++ requestData.py
@@ -23,9 +23,21 @@ loginData = {
 }
 
 # 1st request: POST
+
 # login and request JWT token
 r = requests.post(url = api_endpoint + "/llu/auth/login", headers=headers, json=loginData)
 data = r.json()
 JWT_token = data['data']['authTicket']['token']
-
 print("\nJWT Token:\n" + JWT_token + "\n")
+
+# 2nd request: GET
+
+# update header with JWT token
+extra_header_info = {'authorization': 'Bearer ' + JWT_token}
+headers.update(extra_header_info) 
+
+# request patiendId
+r = requests.get(url = api_endpoint + "/llu/connections", headers=headers)
+data = r.json()
+patient_ID = data['data'][0]['patientId']
+print("\nPatient ID:\n" + patient_ID + "\n")
