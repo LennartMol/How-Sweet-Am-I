0baecb2bf5fb2c1696cb659be40d134517edc508
Lennart Mol <lc.mol@student.avans.nl>
2022-09-14 14:08:01 +0200

    GET request to get data

--- requestData.py
+++ requestData.py
@@ -5,39 +5,40 @@ import privateInfo
 api_endpoint = 'https://api-eu.libreview.io' 
 
 headers= {
-    # general purpose headers
+    # required headers
     'accept-encoding': 'gzip',
     'cache-control': 'no-cache',
     'connection': 'Keep-Alive',
     'content-type': 'application/json',
-
-    # required 
     'product': 'llu.android',
     'version': '4.3.0'
 }
 
+# create 'privateInfo.py' file with variable email and password
 loginData = {
     "email": privateInfo.email,
     "password": privateInfo.password
 }
 
-# 1st request: POST - get JWT token
+def getData():
+    # 1st request: POST - get JWT token
+    r = requests.post(url = api_endpoint + "/llu/auth/login", headers=headers, json=loginData)
+    data = r.json()
+    JWT_token = data['data']['authTicket']['token']
+    print("\nJWT Token:\n" + JWT_token + "\n")
 
-# login and request JWT token
-r = requests.post(url = api_endpoint + "/llu/auth/login", headers=headers, json=loginData)
-data = r.json()
-JWT_token = data['data']['authTicket']['token']
-print("\nJWT Token:\n" + JWT_token + "\n")
+    # 2nd request: GET - get patientId
+    # update header with JWT token
+    extra_header_info = {'authorization': 'Bearer ' + JWT_token}
+    headers.update(extra_header_info) 
+    # request patiendId
+    r = requests.get(url = api_endpoint + "/llu/connections", headers=headers)
+    data = r.json()
+    patient_ID = data['data'][0]['patientId']
+    print("\nPatient ID:\n" + patient_ID + "\n")
 
-# 2nd request: GET - get patientId
-
-# update header with JWT token
-extra_header_info = {'authorization': 'Bearer ' + JWT_token}
-headers.update(extra_header_info) 
-# request patiendId
-r = requests.get(url = api_endpoint + "/llu/connections", headers=headers)
-data = r.json()
-patient_ID = data['data'][0]['patientId']
-print("\nPatient ID:\n" + patient_ID + "\n")
-
-#3rd request: GET - get
+    # 3rd request: GET - get
+    r = requests.get(url = api_endpoint + "/llu/connections/" + patient_ID + "/graph", headers=headers)
+    data = r.json()
+    print(data)
+    return data
