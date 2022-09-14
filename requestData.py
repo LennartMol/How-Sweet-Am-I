55cace8baca9cf48729f9d0c6e3e8f0f6b68b6c0
Lennart Mol <lc.mol@student.avans.nl>
2022-09-14 13:59:05 +0200

    Cleanup

--- requestData.py
+++ requestData.py
@@ -1,4 +1,3 @@
-from urllib import request
 import requests
 import privateInfo
 
@@ -22,7 +21,7 @@ loginData = {
     "password": privateInfo.password
 }
 
-# 1st request: POST
+# 1st request: POST - get JWT token
 
 # login and request JWT token
 r = requests.post(url = api_endpoint + "/llu/auth/login", headers=headers, json=loginData)
@@ -30,14 +29,15 @@ data = r.json()
 JWT_token = data['data']['authTicket']['token']
 print("\nJWT Token:\n" + JWT_token + "\n")
 
-# 2nd request: GET
+# 2nd request: GET - get patientId
 
 # update header with JWT token
 extra_header_info = {'authorization': 'Bearer ' + JWT_token}
 headers.update(extra_header_info) 
-
 # request patiendId
 r = requests.get(url = api_endpoint + "/llu/connections", headers=headers)
 data = r.json()
 patient_ID = data['data'][0]['patientId']
 print("\nPatient ID:\n" + patient_ID + "\n")
+
+#3rd request: GET - get
